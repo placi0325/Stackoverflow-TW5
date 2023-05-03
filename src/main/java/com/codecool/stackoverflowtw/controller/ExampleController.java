3a282c2588cfeb7fc046255c50ceeec607d2cce8
pjung86 <peter.jung.codecool@gmail.com>
2023-05-03 17:07:25 +0200

    display all questions on the main page

--- src/main/java/com/codecool/stackoverflowtw/controller/ExampleController.java
+++ src/main/java/com/codecool/stackoverflowtw/controller/ExampleController.java
@@ -1,5 +1,6 @@
 package com.codecool.stackoverflowtw.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,13 +8,22 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+
+
 @Controller
 @RequestMapping("/")
 public class ExampleController {
+    private final QuestionController questionController;
+
+    @Autowired
+    public ExampleController(QuestionController questionController) {
+        this.questionController = questionController;
+    }
 
     @GetMapping
     public String index(Model model) {
         model.addAttribute("name", "Example name");
+        model.addAttribute("questions", questionController.getAllQuestions());
         return "index";
     }
 
